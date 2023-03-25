c7dcdd8a6a4654c48e09ce3a3142da894a5b6d38
Devontay Eluett <deluett1@gmail.com>
2023-03-25 23:28:30 +0000

    Update UserInfo.js

--- api/UserInfo.js
+++ api/UserInfo.js
@@ -1,5 +1,12 @@
-
+const MongoClient = require("../mongo/client.js");
+const { ObjectId } = require("mongodb");
+const bcrypt = require("bcrypt");
+const { generateToken } = require("../utils/jwt");
+const saltRounds = 10;
+const jwtSecret = process.env.JWT_SECRET;
 
 module.exports = async (req, res) => {
-        console.log("here")
+  console.log("Function started");
+  return;
+ 
 };
