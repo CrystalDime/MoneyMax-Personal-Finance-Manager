7be4a17491233e2313da570e311ab4088748ebaf
Devontay Eluett <deluett1@gmail.com>
2023-03-25 23:45:58 +0000

    Update 2 files

---
+++ src/pages/api/UserInfo.js
@@ -0,0 +1,67 @@
+const MongoClient = require("../mongo/client.js");
+const { ObjectId } = require("mongodb");
+
+const saltRounds = 10;
+const jwtSecret = process.env.JWT_SECRET;
+
+module.exports = async (req, res) => {
+  console.log("Function started");
+  
+  try {
+    const client = await MongoClient();
+    await client.connect();
+    const db = client.db("UserInfo");
+  
+    const { method, query } = req;
+    const { endpoint } = query;
+  
+    console.log(`Endpoint: ${endpoint}`);
+  
+    switch (endpoint) {
+      case "check":
+        console.log("Entering check case");
+        if (method === "POST") {
+          res.status(201).json("Yikadee");
+        }
+        break;
+      case "register":
+        console.log("Entering register case");
+        if (method === "POST") {
+          const { name, email, password } = req.body;
+          const existingUser = await db.collection("users").findOne({ email });
+  
+          if (existingUser) {
+            res.status(409).json({ message: "Email already exists" });
+          } else {
+            const hashedPassword = password
+            const newUser = await db.collection("users").insertOne({
+              name,
+              email,
+              password: hashedPassword,
+            });
+  
+            const token = "token"
+  
+            res.status(201).json({ token, user: { name, email } });
+          }
+        } else {
+          res.status(405).end(`Method ${method} Not Allowed`);
+        }
+        break;
+  
+      // ... rest of the cases
+  
+      default:
+        res.status(400).json({ message: "Invalid endpoint" });
+        client.close();
+        return;
+    }
+  
+    // ... rest of the code
+  } catch (error) {
+    console.error("Error occurred:", error);
+    res.status(500).json({ message: "Internal server error" });
+  } finally {
+    console.log("Function finished");
+  }
+};
