901410dc6a5fe0e60192c440e8693ea218c51781
Devontay Eluett <deluett1@gmail.com>
2023-03-26 16:09:03 +0000

    Update UserInfo.js

--- api/UserInfo.js
+++ api/UserInfo.js
@@ -1,80 +1,118 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import { ObjectId } from 'mongodb';
 
-
 const username = encodeURIComponent('devontay');
 const password = encodeURIComponent('eluett');
 const uri = `mongodb+srv://${username}:${password}@moneymax.1edvk23.mongodb.net/?retryWrites=true&w=majority`;
 console.log(username);
 console.log(password);
 
-
-
-
-
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 
 export default async function UserInfo(req, res) {
   console.log("Function started");
   
   try {
-    await client.connect(); // Modify this line
+    await client.connect();
     const db = client.db("UserInfo");
-  
+
     const { method, query } = req;
     const { endpoint } = query;
-  
+
     console.log(`Endpoint: ${endpoint}`);
-    console.log(method);
-    console.log(req);
+
     switch (endpoint) {
       case "check":
-        console.log("Entering check case");
-        if (method === "GET") {
-          res.status(201).json("Yikadee");
+        case "check":
+          console.log("Entering check case");
+          if (method === "GET") {
+            res.status(201).json("Yikadee");
+          }
+          break;
+
+      case "login":
+        // Handle user login here
+        if (method === "POST") {
+          const { email, password } = req.body;
+          const user = await db.collection("users").findOne({ email });
+
+          if (!user) {
+            res.status(404).json({ message: "User not found" });
+          } else {
+            const isPasswordCorrect = password === user.password;
+
+            if (isPasswordCorrect) {
+              const token = "token";
+              res.status(200).json({ token, user: { name: user.name, email: user.email } });
+            } else {
+              res.status(401).json({ message: "Invalid credentials" });
+            }
+          }
+        } else {
+          res.status(405).end(`Method ${method} Not Allowed`);
         }
         break;
+
       case "register":
         console.log("Entering register case");
         if (method === "POST") {
           const { name, email, password } = req.body;
           const existingUser = await db.collection("users").findOne({ email });
-  
+
           if (existingUser) {
             res.status(409).json({ message: "Email already exists" });
           } else {
-            // Modify this line
+            const hashedPassword = password;
             const newUser = await db.collection("users").insertOne({
               name,
               email,
-              password: password,
+              password: hashedPassword,
             });
-  
-            const token = "token"
-  
+
+            const token = "";
             res.status(201).json({ token, user: { name, email } });
           }
         } else {
           res.status(405).end(`Method ${method} Not Allowed`);
         }
         break;
-  
-      // ... rest of the cases
-  
+
+      case "users":
+        // Handle user-related operations here
+        break;
+
+      case "incomes":
+        // Handle income-related operations here
+        break;
+
+      case "expenses":
+        // Handle expense-related operations here
+        break;
+
+      case "savings_goals":
+        // Handle savings goal-related operations here
+        break;
+
       default:
         res.status(400).json({ message: "Invalid endpoint" });
         client.close();
         return;
     }
-  
-    // ... rest of the code
-  } catch (error) {
-    console.error("Error occurred:", error);
-    res.status(500).json({ message: "Internal server error" });
-  } finally {
-    console.log("Function finished");
-  }
-};
 
+   
+      
+      client.close();
+  } catch (error) {
+      console.error("Error occurred:", error);
+      res.status(500).json({ message: "Internal server error" });
+  } finally {
+      console.log("Function finished");
+ }
+};
+      
+    
+      
+      
+      
+      
+      
