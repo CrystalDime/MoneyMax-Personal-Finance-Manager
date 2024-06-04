b370f99e64cb700380a681942680533815230858
Devontay Eluett <45443045+CrystalDime@users.noreply.github.com>
2024-06-04 17:31:48 -0400

    Update client.js

--- mongo/client.js
+++ mongo/client.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = "mongodb+srv://deluett1@gmail.com:Nanaosaki12@moneymax.1edvk23.mongodb.net/?retryWrites=true&w=majority"
+const uri = "mongodb+srv://deluett1@gmail.com@moneymax.1edvk23.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = async () => {
